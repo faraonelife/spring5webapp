634316fe3f1d86feccc5a084190328b8ae293c53
Farxod Khaitbaev <72006448+faraonelife@users.noreply.github.com>
2021-09-06 12:24:09 +0500

    Delete src/main/java/com/faraonelife/spring6webapp directory

--- src/main/java/com/faraonelife/spring6webapp/repositories/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.faraonelife.spring6webapp.repositories;
-import com.faraonelife.spring6webapp.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository  extends CrudRepository<Author,Long> {
-}
--- src/main/java/com/faraonelife/spring6webapp/repositories/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.faraonelife.spring6webapp.repositories;
-
-import com.faraonelife.spring6webapp.domain.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
